ee2f8e2ec917284eb2c0a7a83d804cda3689dd09
Your Name <you@example.com>
2024-03-11 13:10:59 +0000

    change user

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,7 +1,7 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/micha-bitton/ci-cd.git', 'main') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL NodeJs User Example')
+        git('https://github.com/daniklevak/ci-cd.git', 'main') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL FUN LAB')
             node / gitConfigEmail('jenins-dsl@domain.com')
         }
     }
